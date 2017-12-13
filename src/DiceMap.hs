89bd39f81c2ac2da32154cb3e853c779a55f83c4
jk977 <jdk64@comcast.net>
2017-12-13 00:49:57 -0800

    Using Data.Map for DiceMap

--- src/DiceMap.hs
+++ src/DiceMap.hs
@@ -2,15 +2,17 @@ module DiceMap where
 
 import Dice
 import Point
+
 import Data.List (permutations)
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 type DiceKey = (Point,Die)
-type DiceMap = [(DiceKey,Int)]
+type DiceMap = Map DiceKey Int
 
 blankMap :: DiceMap
-blankMap = states where
+blankMap = Map.fromList states where
     points = [Point x y | x <- cycle [1..60], y <- concat $ replicate 60 <$> [1..60]]
     dice = cycle [Die a b c d e f | [a,b,c,d,e,f] <- permutations [1..6]]
     possibilities = zip points dice
     states = zip possibilities $ repeat (-1)
-
